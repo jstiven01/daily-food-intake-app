67f5f6e4c8d153078aefb04ffde9e604b0535589
jstiven01 <stiven10@gmail.com>
2020-02-02 08:22:14 -0500

    TDD Implementig Nutrient Model

--- app/models/nutrient.rb
+++ app/models/nutrient.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Nutrient < ApplicationRecord
+    # validations
+    validates_presence_of :name, :units, :date_progress
+
+    # Associations
+    has_many :measurements, dependent: :destroy
 end
--- spec/models/nutrient_spec.rb
+++ spec/models/nutrient_spec.rb
@@ -2,6 +2,14 @@
 
 require 'rails_helper'
 
+# Test suite for the Nutrient model
 RSpec.describe Nutrient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  
+  it { should have_many(:measurements).dependent(:destroy) }
+  # Validation tests
+  
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:units) }
+  it { should validate_presence_of(:date_progress) }
 end
