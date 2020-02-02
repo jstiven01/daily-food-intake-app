782b1fe54e2f36150ce48abba8df1c13b6e6c2df
jstiven01 <stiven10@gmail.com>
2020-02-02 12:13:02 -0500

    TDD Implementing Measurement API

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,4 +1,48 @@
 # frozen_string_literal: true
 
 class MeasurementsController < ApplicationController
+  before_action :set_nutrient
+  before_action :set_nutrient_measurement, only: %i[show update destroy]
+
+  # GET /nutrients/:nutrient_id/measurements
+  def index
+    json_response(@nutrient.measurements)
+  end
+
+  # GET /nutrients/:nutrient_id/measurements/:id
+  def show
+    json_response(@measurement)
+  end
+
+  # POST /nutrients/:nutrient_id/measurements
+  def create
+    @nutrient.measurements.create!(measurement_params)
+    json_response(@nutrient, :created)
+  end
+
+  # PUT /nutrients/:nutrient_id/measurements/:id
+  def update
+    @measurement.update(measurement_params)
+    head :no_content
+  end
+
+  # DELETE /nutrients/:nutrient_id/measurements/:id
+  def destroy
+    @measurement.destroy
+    head :no_content
+  end
+
+  private
+
+  def measurement_params
+    params.permit(:amount, :date_intake)
+  end
+
+  def set_nutrient
+    @nutrient = Nutrient.find(params[:nutrient_id])
+  end
+
+  def set_nutrient_measurement
+    @measurement = @nutrient.measurements.find_by!(id: params[:id]) if @nutrient
+  end
 end
