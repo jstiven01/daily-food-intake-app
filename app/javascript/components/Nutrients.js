e952ede09a74366ea16a7b1656a9531238280fa1
jstiven01 <stiven10@gmail.com>
2020-02-07 13:43:36 -0500

    Adding circular progress bar

--- app/javascript/components/Nutrients.js
+++ app/javascript/components/Nutrients.js
@@ -2,12 +2,15 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import { getNutrients } from '../redux/nutrients/actions';
+import 'react-circular-progressbar/dist/styles.css';
 
 const Nutrients = ({ nutrientsData, getNutrients }) => {
   useEffect(() => {
     getNutrients();
   }, []);
+
 
   const jsxNutrients = nutrientsData.map(nutrient => (
     <div key={nutrient.id} className="nutrient">
@@ -31,6 +34,37 @@ const Nutrients = ({ nutrientsData, getNutrients }) => {
   ));
 
 
+  let totalProtein;
+  let totalCarbs;
+  let totalFat;
+  if (nutrientsData.length > 0) {
+    const protein = nutrientsData.filter(nutrient => nutrient.name === 'Protein'
+    && nutrient.date_progress.substring(0, 10) === new Date().toISOString().substring(0, 10))[0];
+    if (!protein) {
+      totalProtein = 0;
+    } else {
+      totalProtein = protein.total_nutrient;
+    }
+    const carbs = nutrientsData.filter(nutrient => nutrient.name === 'Carbs'
+    && nutrient.date_progress.substring(0, 10) === new Date().toISOString().substring(0, 10))[0];
+    if (!carbs) {
+      totalCarbs = 0;
+    } else {
+      totalCarbs = carbs.total_nutrient;
+    }
+    const fat = nutrientsData.filter(nutrient => nutrient.name === 'Fat'
+    && nutrient.date_progress.substring(0, 10) === new Date().toISOString().substring(0, 10))[0];
+    if (!fat) {
+      totalFat = 0;
+    } else {
+      totalFat = fat.total_nutrient;
+    }
+  }
+
+  const percentageProtein = Math.round((totalProtein / 50) * 100);
+  const percentageCarbs = Math.round((totalCarbs / 300) * 100);
+  const percentageFat = Math.round((totalFat / 100) * 100);
+  const today = new Date();
   return (
 
     <div className="col-12 p-0">
@@ -38,7 +72,58 @@ const Nutrients = ({ nutrientsData, getNutrients }) => {
         <h2 className="top-title">My Nutrients</h2>
       </div>
       <div className="col-12 section-statistics">
-        statistics
+        <div className="col-12 date-main text-center pt-3">
+          <span className="main-size-date">
+            {new Intl.DateTimeFormat('en-GB', {
+              year: 'numeric',
+              month: 'long',
+              day: '2-digit',
+            }).format(today)}
+          </span>
+
+        </div>
+        <div className="d-flex mx-4">
+          <div className="d-flex flex-column text-center">
+            <CircularProgressbar
+              className="circle-progress align-self-center"
+              value={percentageProtein}
+              text={`${percentageProtein}%`}
+              styles={buildStyles({
+                textColor: '#8395A5',
+                pathColor: '#ADDC91',
+                trailColor: '#E8FCFF',
+              })}
+            />
+            <span className="span-name-statistics">Protein</span>
+          </div>
+          <div className="d-flex flex-column text-center">
+            <CircularProgressbar
+              className="circle-progress align-self-center"
+              value={percentageCarbs}
+              text={`${percentageCarbs}%`}
+              styles={buildStyles({
+                textColor: '#8395A5',
+                pathColor: '#ADDC91',
+                trailColor: '#E8FCFF',
+              })}
+            />
+            <span className="span-name-statistics">Carbs</span>
+          </div>
+          <div className="d-flex flex-column text-center">
+            <CircularProgressbar
+              className="circle-progress align-self-center"
+              value={percentageFat}
+              text={`${percentageFat}%`}
+              styles={buildStyles({
+                textColor: '#8395A5',
+                pathColor: '#ADDC91',
+                trailColor: '#E8FCFF',
+              })}
+            />
+            <span className="span-name-statistics">Fat</span>
+          </div>
+        </div>
+
       </div>
       <div className="col-12 nutrients-global">
         {jsxNutrients}
