f5077421c795d53d6acd317901a0538244b4130b
jstiven01 <stiven10@gmail.com>
2020-02-11 08:03:12 -0500

    Deleting print on jwt method to chech credentials

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -6,7 +6,6 @@ class JsonWebToken
     # set expiry to 24 hours from creation time
     payload[:exp] = exp.to_i
     # sign token with application secret
-    p 'SECREEETTT',payload, HMAC_SECRET
     JWT.encode(payload, HMAC_SECRET)
   end
 
